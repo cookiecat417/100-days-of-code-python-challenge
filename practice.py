2cfd64aa8b058565f53b9013e8e17896e9aea53f
cookiecat <kpeyton417@gmail.com>
2026-06-14 12:32:09 -0700

    Finished Day 1 Project: Band Name Generator

---
+++ practice.py
@@ -0,0 +1,4 @@
+print("Welcome, user! Please fill out the following information:")
+city = input("What is the name of the city you grew up in?\n ")
+pet = input("What is the name of a pet that you own?\n ")
+print("Thank you for your participation! The name of your band should be the " + city + " " + pet + "!")
