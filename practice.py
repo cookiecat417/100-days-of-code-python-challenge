7cc3087fb73f55e19ac1ea4dd1bd2b44d162c30c
cookiecat <kpeyton417@gmail.com>
2026-06-14 12:53:53 -0700

    Python Primitive Data Types

--- practice.py
+++ practice.py
@@ -1,4 +1,23 @@
-print("Welcome, user! Please fill out the following information:")
-city = input("What is the name of the city you grew up in?\n ")
-pet = input("What is the name of a pet that you own?\n ")
-print("Thank you for your participation! The name of your band should be the " + city + " " + pet + "!")
+# Subscripting
+print("Hello"[0])
+
+
+# String
+print("123" + "345")
+
+
+# Integer = Whole Number
+print(123 + 345)
+
+
+# Large Integers
+print(123_456_789)
+
+
+# Float = Floating Point Number
+print(3.14159)
+
+
+# Boolean 
+print(True) 
+print(False)
